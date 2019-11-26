b4c42c7c496800a8d67b1bc683da504efb4bf160
Thorsten Hofmann <thorsten.hofmann@telekom.de>
2019-11-26 15:38:45 +0100

    first draft

---
+++ Game.java
@@ -0,0 +1,16 @@
+class Game {
+    public static void main(String[] args) {
+
+        Dealer dealer = new Dealer("Dealer");
+        Player player = new Player("Player");
+
+        int counter = 1;
+
+        while (counter < 3) {
+            draw
+            counter++;
+        }
+
+
+    }
+}
