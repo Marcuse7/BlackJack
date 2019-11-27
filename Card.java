d9bacda03767fba4b01fe4a0699ef089993c0a1b
Stuchlikom <stuchlikom@gmail.com>
2019-11-27 13:42:49 +0100

    Card & Stack umgebaut auf ASCII-Reihenfolge / toName mit Kartenausgabe als String hinzugefügt

--- Card.java
+++ Card.java
@@ -4,6 +4,9 @@ public class Card {
     private int number;
     private int color;
     private int value;
+    private String cardName;
+    private int symbol;
+
 
     // constructors
     public Card(int number, int color, int value) {
@@ -34,4 +37,33 @@ public class Card {
         this.value = value;
     }
 
+    public String toName() {
+
+
+        //this.number;
+        if (color == 1 || color == 2) {
+            cardName = "\u001B[47m\u001B[31m";
+        } else {
+            cardName = "\u001B[47m\u001B[30m";
+        }
+
+        switch (color) {
+            case 0:
+                symbol = 0;
+                break;
+            case 1:
+                symbol = 16;
+                break;
+            case 2:
+                symbol = 32;
+                break;
+            case 3:
+                symbol = 48;
+                break;
+        }
+        
+        cardName = cardName + new String(new int[] {0x1F0A1 + number + symbol}, 0, 1) + " \u001B[0m";
+
+        return cardName;
+    }
 }
