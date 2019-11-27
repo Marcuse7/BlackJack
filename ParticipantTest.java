b8c73f256812e3f381b20c60e585b4a6e79fead2
Kadir <abdulkadir.erucu@telekom.de>
2019-11-27 13:36:11 +0100

    ParticipantTest

---
+++ ParticipantTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ParticipantTest {
+
+    @Test
+    public void testTrue() throws Exception {
+        Assert.assertEquals(true, true);
+    }
+
+    // TODO
+}
