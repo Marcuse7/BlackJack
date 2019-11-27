2f052c7eefb9d38d141566f7bfdec239d7e5a322
Kadir <abdulkadir.erucu@telekom.de>
2019-11-27 11:27:24 +0100

    Participant.java ergänzt.

--- Participant.java
+++ Participant.java
@@ -1,20 +1,28 @@
 import java.util.ArrayList;
 
-class Paricipant{
+class Participant{
 
-    private ArrayList<Card> cards;
+    private ArrayList<Card> cardsHand;
 
     public String toString(){
+        String allCards = "";
+        for (Card card: cardsHand){
+        allCards = allCards + card.toString();
 
+        } // Ende for
+        return allCards;
     } // Ende toString
 
     public int getSum(){
-
-
+        int sum = 0;
+        for (Card card: cardsHand){
+            sum = sum + card.getValue();
+        } // Ende for
+        return sum;
     } // Ende getSum
 
-    public void takeCard(Card){
-
+    public void takeCard(Card card){
+        cardsHand.add(card);
 
     } // Ende takeCard
 
