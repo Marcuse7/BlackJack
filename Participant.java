1c5d79ccc36a0e96bdf2a09e82fb9927fb49cf11
Kadir <abdulkadir.erucu@telekom.de>
2019-11-27 11:03:31 +0100

    UML und Participant.java

---
+++ Participant.java
@@ -0,0 +1,21 @@
+class Paricipant{
+
+    private ArrayList<Card> cards;
+
+    public String toString(){
+
+    } // Ende toString
+
+    public int getSum(){
+
+
+    } // Ende getSum
+
+    public void takeCard(Card){
+
+
+    } // Ende takeCard
+
+
+
+} // Ende class
