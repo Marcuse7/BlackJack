b143bbd686bda9e3efb296da981a55bc7b6f2309
Kadir <abdulkadir.erucu@telekom.de>
2019-11-27 14:00:25 +0100

    ParticipantTest

--- ParticipantTest.java
+++ ParticipantTest.java
@@ -2,10 +2,14 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class ParticipantTest {
-
+Participant participant = new Participant();
+Card testCard = new Card(13, 1, 10);
     @Test
     public void testTrue() throws Exception {
-        Assert.assertEquals(true, true);
+        Assert.assertEquals(0, participant.getSum());
+        Assert.assertEquals("", participant.toString());
+        participant.takeCard(testCard);
+        Assert.assertEquals(10, participant.getSum());
     }
 
     // TODO
